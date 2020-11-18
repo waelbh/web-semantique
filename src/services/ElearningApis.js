03448045bcc28efed38365c6a14f268090df0d81
waelbh <47089212+waelbh@users.noreply.github.com>
2020-11-18 21:35:36 +0100

    services

--- src/services/ElearningApis.js
+++ src/services/ElearningApis.js
@@ -3,10 +3,121 @@ import axios from "axios";
 
 
 
-export function getAllCountries() {
+export function getProfessorExams() {
   return new Promise((resolve, reject) => {
     axios
-      .get()
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/profsByExam")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+export function getProfessorEtudiants() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/profsByEtudiants")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+export function getProfessorCours() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/profsByCours")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+export function getModulesByMeets() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/modulesByMeets")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+export function getModulesByCours() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/modulesByCours")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+export function getModulesByDocument() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/modulesByDocument")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+export function getAllProfessor() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/allProfs")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+export function getAllEtudiant() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/Etudiant")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+export function getAllCours() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/allCours")
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+export function getAllModule() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:9080/elearning-project-0.0.1-SNAPSHOT/rest/Elearnig/allmodules")
       .then(response => {
         resolve(response.data);
       })
